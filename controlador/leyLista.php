d48ee7b9343552aeec25b28c0b1c5abfafff2e71
0xdiego <93290842+diegojoel301@users.noreply.github.com>
2023-09-07 04:14:26 +0200

    Update leyLista.php

--- controlador/leyLista.php
+++ controlador/leyLista.php
@@ -1,7 +1,10 @@
 <?php
+    include("../vista/dashboard_admin/head.php");
+    include("../vista/dashboard_admin/sidebar.php");
     //incluimos modelo
     include("../modelo/Ley_NormativaClase.php");
     $car=new Ley_Normativa("","","","","");
     $res=$car->lista();
     include("../vista/LeyLista.php");
+    include("../vista/dashboard_admin/footer.php");
 ?>

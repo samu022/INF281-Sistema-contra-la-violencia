1f749d3b2c0cffb2ae93d8b22accbac01cc38225
0xdiego <93290842+diegojoel301@users.noreply.github.com>
2023-09-07 04:14:47 +0200

    Create dashboard.php

---
+++ controlador/dashboard.php
@@ -0,0 +1,8 @@
+<?php
+
+    include("../vista/dashboard_admin/head.php");
+    include("../vista/dashboard_admin/sidebar.php");
+    include("../vista/dashboard_admin/dashboard.php");
+
+    include("../vista/dashboard_admin/footer.php");
+?>

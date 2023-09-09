8ec23b696501c73b7625493923db0b23e9d9ace8
samu022 <samuelalegre2002gmail.com>
2023-09-09 10:14:30 -0400

    var char de 500 a la descripción de la denuncia

--- bdcontraviolencia.sql
+++ bdcontraviolencia.sql
@@ -324,7 +324,7 @@ CREATE TABLE `persona` (
 CREATE TABLE `prueba` (
   `codPrueba` int(11) NOT NULL,
   `tipo` varchar(40) DEFAULT NULL,
-  `descripcion` varchar(40) DEFAULT NULL,
+  `descripcion` varchar(500) DEFAULT NULL,
   `ruta` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 

7d1a6aa0b94a261da56d1cb527edaab07b3d7d00
samu022 <samuelalegre2002gmail.com>
2023-09-09 11:18:15 -0400

    prueba clase

--- vista/Reporte_denuncias/prueba.php
+++ vista/Reporte_denuncias/prueba.php
@@ -8,13 +8,19 @@
     <title>Pruebas</title>
 </head>
 <body>
+  <form action="tu_archivo_php.php" method="POST" enctype="multipart/form-data">
     <section class="form-register">
       <h4>Pruebas de la Denuncia</h4>
+
       <input type="file" name="form_fields[field_3fc0355]" id="form-field-field_3fc0355" class="archivo" data-maxsize="2" data-maxsize-message="This file exceeds the maximum allowed size.">
-      <textarea class="descripcion" name="form_fields[message]" id="form-field-message" rows="4" placeholder="Describa las pruebas aqui." required="required" aria-required="true"></textarea>
-    </section>  
-    <!--<footer>
+      <textarea class="descripcion" name="form_fields[message]" id="form-field-message" rows="4" placeholder="Describa las pruebas aquí." required="required" aria-required="true"></textarea>
+
+      <input class="botons" type="submit" value="Subir Prueba">
+    </section>
+  </form>
+  <!--<footer>
         <center>© 2023 Mi Página Web de Denuncias</center>
     </footer>-->
 </body>
+
 </html>

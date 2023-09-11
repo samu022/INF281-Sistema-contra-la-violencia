43b368108014e12509c80bc16e72700da128e33c
0xdiego <93290842+diegojoel301@users.noreply.github.com>
2023-09-11 09:28:58 +0200

    Create admnistradorListar.php

---
+++ controlador/admnistradorListar.php
@@ -0,0 +1,18 @@
+<?php
+
+    session_start();
+
+    if($_SESSION['privilegio'] == "usuario" || $_SESSION['privilegio'] == "")
+    {
+        header("Location: ../controlador/login.php");
+    }
+
+    include("../vista/dashboard_admin/head.php");
+    include("../vista/dashboard_admin/sidebar.php");
+    //incluimos modelo
+    include("../modelo/administrador.php");
+    $car=new Administrador("","","","","");
+    $res=$car->lista();
+    include("../vista/administradorLista.php");
+    include("../vista/dashboard_admin/footer.php");
+?>

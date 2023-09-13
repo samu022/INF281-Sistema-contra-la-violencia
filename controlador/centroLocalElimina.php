369db4dfe2de4e554141d1fa81400f448977319a
0xdiego <93290842+diegojoel301@users.noreply.github.com>
2023-09-13 16:35:49 +0200

    Update centroLocalElimina.php

--- controlador/centroLocalElimina.php
+++ controlador/centroLocalElimina.php
@@ -1,4 +1,14 @@
 <?php
+session_start();
+
+    if($_SESSION['privilegio'] == "lectura")
+    {
+        header("Location: ../controlador/dashboard.php");
+    }
+    else if($_SESSION['privilegio'] == "usuario" || $_SESSION['privilegio'] == "")
+    {
+        header("Location: ../controlador/login.php");
+    }
     $codCentro=$_GET['cod'];
     include("../modelo/CentroLocalClase.php");
     $carg=new CentroLocal($codCentro,"","","","");

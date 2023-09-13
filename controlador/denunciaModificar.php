3e3432d474bff43e0cb27c7c9c46f87fd7056c71
0xdiego <93290842+diegojoel301@users.noreply.github.com>
2023-09-13 16:38:12 +0200

    Update denunciaModificar.php

--- controlador/denunciaModificar.php
+++ controlador/denunciaModificar.php
@@ -1,4 +1,14 @@
 <?php
+session_start();
+
+    if($_SESSION['privilegio'] == "lectura")
+    {
+        header("Location: ../controlador/dashboard.php");
+    }
+    else if($_SESSION['privilegio'] == "usuario" || $_SESSION['privilegio'] == "")
+    {
+        header("Location: ../controlador/login.php");
+    }
 include("../modelo/conexion.php");
 $cod = $_GET['cod'];
 include("../modelo/DenunciaClase.php");

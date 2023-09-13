22b7434716932b4e5ed37cb2dd4f8be1047c4ea0
0xdiego <93290842+diegojoel301@users.noreply.github.com>
2023-09-13 16:36:38 +0200

    Update centroLocalRegistro.php

--- controlador/centroLocalRegistro.php
+++ controlador/centroLocalRegistro.php
@@ -1,4 +1,14 @@
 <?php
+session_start();
+
+    if($_SESSION['privilegio'] == "lectura")
+    {
+        header("Location: ../controlador/dashboard.php");
+    }
+    else if($_SESSION['privilegio'] == "usuario" || $_SESSION['privilegio'] == "")
+    {
+        header("Location: ../controlador/login.php");
+    }
     include("../vista/CentroLocalRegistro.php");
     if(isset($_POST['RegistrarCentro'])){
         $nombre=$_POST['nombre'];

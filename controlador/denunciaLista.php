ad547f0d03858f02dc9a2792938fe696334e6e14
0xdiego <93290842+diegojoel301@users.noreply.github.com>
2023-09-13 16:55:29 +0200

    Update denunciaLista.php

--- controlador/denunciaLista.php
+++ controlador/denunciaLista.php
@@ -5,6 +5,8 @@ session_start();
     {
         header("Location: ../controlador/login.php");
     }
+ include("../vista/dashboard_admin/head.php");
+    include("../vista/dashboard_admin/sidebar.php");
     include("../modelo/conexion.php");
     //incluimos modelo
     include("../modelo/DenunciaClase.php");

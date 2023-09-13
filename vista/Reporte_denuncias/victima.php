657c2e5359959c159120a131f4eed423b7165201
Den-Flores <143958118+Den-Flores@users.noreply.github.com>
2023-09-13 09:11:28 -0400

    Cambios victima.php

--- vista/Reporte_denuncias/victima.php
+++ vista/Reporte_denuncias/victima.php
@@ -1,39 +1,67 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-with, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="styles.css">
     <title>Datos de la Victima</title>
 </head>
 <body>
     <section class="form-register">
       <h4>Datos de la Victima</h4>
-      <form action="" method="POST">
-      <input class="controls" type="text" name="nombres" id="nombres" placeholder="Ingrese nombre" required>
-      <input class="controls" type="text" name="apellidoP" id="apellidoP" placeholder="Ingrese apellido Paterno" required>
-      <input class="controls" type="text" name="apellidoM" id="apellidoM" placeholder="Ingrese apellido Materno">
-      <label for="fechaNacimiento">Fecha de Nacimiento:</label>
-      <input class="controls" type="date" name="fechaNacimiento" id="fechaNacimiento" required>
-      <label for="sexo">Sexo de la victima:</label>
+      <form action="" class="formulario" method="POST">
+      <div class="formulario__grupo-input">
+            <label for="nombre">Nombres:</label>
+            <input class="controls" type="text" name="nombres" id="nombres" placeholder="Ingrese nombre agresor" required>
+        </div>
+        <div class="formulario__grupo-input">
+            <label for="paterno">Apellido Paterno:</label>
+            <input class="controls" type="text" name="apellidoP" id="apellidoP" placeholder="Ingrese apellido Paterno" required>
+        </div>
+        <div class="formulario__grupo-input">
+            <label for="Materno">Apellido Materno:</label>
+            <input class="controls" type="text" name="apellidoM" id="apellidoM" placeholder="Ingrese apellido Materno">
+        </div>
+        <div class="formulario__grupo-input">
+            <label for="fechaNacimiento">Fecha de Nacimiento:</label>
+            <input class="controls" type="date" name="fechaNacimiento" id="fechaNacimiento Agresor" required>
+        </div>
+        <div class="formulario__grupo-input">
+            <label for="sexo">Sexo de la victima:</label>
             <select class="controls" name="sexo" id="sexo" required>
                 <option value="Masculino">Masculino</option>
                 <option value="Femenino">Femenino</option>
             </select>
-      <input class="controls" type="text" name="dir" id="dir" placeholder="Ingrese domicilio victima">
-      <input class="controls" type="text" name="est" id="est" placeholder="Ingrese estado civil victima">
-      <input class="controls" type="text" name="prof" id="prof" placeholder="Ingrese profesion victima">
-      <input class="controls" type="text" name="carnet" id="carnet" placeholder="Ingrese su carnet de identidad" required>
-      <input class="controls" type="text" name="rel" id="rel" placeholder="Relacion Perpetruador" required>
-      <input class="controls" type="text" name="telefono" id="telefono" placeholder="Ingrese su numero telefonico">
-      <p>Estoy de acuerdo con <a href="#">Terminos y Condiciones</a></p>
-      <input class="btn btn-primary" type="submit" value="Registrar Víctima" name="registraVictima">
-
+        </div>
+        <div class="formulario__grupo-input">
+            <label for="domicilioVictima">Domicilio:</label>
+            <input class="controls" type="text" name="dir" id="dir" placeholder="Ingrese domicilio victima">
+        </div>
+        <div class="formulario__grupo-input">
+            <label for="estadoCivil">Estado Civil:</label>
+            <input class="controls" type="text" name="est" id="est" placeholder="Ingrese estado civil victima">
+        </div>
+        <div class="formulario__grupo-input">
+            <label for="profesion">Profesion:</label>
+            <input class="controls" type="text" name="prof" id="prof" placeholder="Ingrese profesion victima">
+        </div>
+        <div class="formulario__grupo-input">
+            <label for="carnet">Carnet de Identidad:</label>
+            <input class="controls" type="text" name="carnet" id="carnet" placeholder="Ingrese su carnet de identidad" required>
+        </div>
+        <div class="formulario__grupo-input">
+            <label for="relacion">Relacion con el perpetrador:</label>
+            <input class="controls" type="text" name="rel" id="rel" placeholder="familiar,pareja,amistad..." required>
+        </div>
+        <div class="formulario__grupo-input">
+            <label for="telefono">Telefono:</label>
+            <input class="controls" type="text" name="telefono" id="telefono" placeholder="Ingrese su numero telefonico">
+        </div>
+      
     </form>
+        <p>Estoy de acuerdo con <a href="#">Terminos y Condiciones</a></p>
+        <input class="botons" type="submit" value="Registrar Víctima" name="registraVictima">
     </section>  
-    <!--<footer>
-        © 2023 Mi Página Web de Denuncias
-    </footer>-->
 </body>
 </html>

81582d7dc9417aeaaf05bb2e33a8396ee6663900
Den-Flores <143958118+Den-Flores@users.noreply.github.com>
2023-09-13 09:22:06 -0400

    Cambio prueba.php

--- vista/Reporte_denuncias/prueba.php
+++ vista/Reporte_denuncias/prueba.php
@@ -5,62 +5,27 @@
     <meta name="viewport" content="width=device-with, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../css/styles.css">
-    <title>Datos del Agresor</title>
+    <title>Pruebas</title>
 </head>
 <body>
+  <form action="" method="POST" enctype="multipart/form-data">
     <section class="form-register">
-      <h4>Datos del Agresor</h4>
-      <form action="" class="formulario" method="POST">
-        <div class="formulario__grupo-input">
-            <label for="nombre">Nombres:</label>
-            <input class="controls" type="text" name="nombres" id="nombres" placeholder="Ingrese nombre agresor" required>
-        </div>
-        <div class="formulario__grupo-input">
-            <label for="paterno">Apellido Paterno:</label>
-            <input class="controls" type="text" name="apellidoP" id="apellidoP" placeholder="Ingrese apellido Paterno" required>
-        </div>
-        <div class="formulario__grupo-input">
-            <label for="Materno">Apellido Materno:</label>
-            <input class="controls" type="text" name="apellidoM" id="apellidoM" placeholder="Ingrese apellido Materno">
-        </div>
-        <div class="formulario__grupo-input">
-            <label for="fechaNacimiento">Fecha de Nacimiento:</label>
-            <input class="controls" type="date" name="fechaNacimiento" id="fechaNacimiento Agresor" required>
-        </div>
-        <div class="formulario__grupo-input">
-            <label for="sexo">Sexo del agresor:</label>
-            <select class="controls" name="sexo" id="sexo" required>
-                <option value="Masculino">Masculino</option>
-                <option value="Femenino">Femenino</option>
-            </select>
-        </div>
-        <div class="formulario__grupo-input">
-            <label for="domicilioAgresor">Domicilio:</label>
-            <input class="controls" type="text" name="dir" id="dir" placeholder="Ingrese domicilio agresor" required>
-        </div>
-        <div class="formulario__grupo-input">
-            <label for="estadoCivil">Estado Civil:</label>
-            <input class="controls" type="text" name="est" id="est" placeholder="Ingrese estado civil agresor">
-        </div>
-        <div class="formulario__grupo-input">
-            <label for="profesion">Profesion:</label>
-            <input class="controls" type="text" name="prof" id="prof" placeholder="Ingrese profesion agresor">
-        </div>
-        <div class="formulario__grupo-input">
-            <label for="carnet">Carnet de Identidad:</label>
-            <input class="controls" type="text" name="carnet" id="carnet" placeholder="Ingrese su carnet de identidad" required>
-        </div>
-        <div class="formulario__grupo-input">
-            <label for="telefono">Telefono:</label>
-            <input class="controls" type="text" name="tel" id="tel" placeholder="Telefono" >
-        </div>
-        <div class="formulario__grupo-input">
-            <label for="descripcion">Descripcion:</label>
-            <textarea class="descripcion" name="form_fields[message]" id="form-field-message" rows="4" placeholder="Describa las pruebas aqui." required="required" aria-required="true"></textarea>
-        </div>
-    </form>
-        <p>Estoy de acuerdo con <a href="#">Terminos y Condiciones</a></p>
-        <input class="botons" type="submit" value="Registrar Agresor" name="registrarAgresor">
-    </section>  
+      <h4>Pruebas de la Denuncia</h4>
+
+      <label for="imagen">Imagen:</label>
+      <input type="file" name="form_fields[field_3fc0355]" id="form-field-field_3fc0355" class="archivo" data-maxsize="2" data-maxsize-message="This file exceeds the maximum allowed size.">
+      <textarea class="descripcion" name="form_fields[message]" id="form-field-message" rows="4" placeholder="Descripcion de las pruebas." required="required" aria-required="true"></textarea>
+
+      <label for="video">Video:</label>
+      <input type="file" name="form_fields[field_3fc0355]" id="form-field-field_3fc0355" class="archivo" data-maxsize="2" data-maxsize-message="This file exceeds the maximum allowed size.">
+      <textarea class="descripcion" name="form_fields[message]" id="form-field-message" rows="4" placeholder="Descripcion de las pruebas." required="required" aria-required="true"></textarea>
+      
+      <label for="audio">Audio:</label>
+      <input type="file" name="form_fields[field_3fc0355]" id="form-field-field_3fc0355" class="archivo" data-maxsize="2" data-maxsize-message="This file exceeds the maximum allowed size.">
+      <textarea class="descripcion" name="form_fields[message]" id="form-field-message" rows="4" placeholder="Descripcion de las pruebas." required="required" aria-required="true"></textarea>
+
+      <input class="botons" type="submit" value="Subir Prueba" name="subePrueba">
+    </section>
+  </form>
 </body>
 </html>

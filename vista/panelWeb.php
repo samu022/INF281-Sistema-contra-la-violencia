ac790c563b874ac48ec3c2f478f57cb2774f5dd2
samu022 <samuelalegre2002gmail.com>
2023-09-15 15:42:12 -0400

    panelWeb v1

---
+++ vista/panelWeb.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Contra la Violencia</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <style>
+        /* Estilo personalizado para el pie de página */
+        footer {
+            background-color: #333;
+            color: white;
+            text-align: center;
+            padding: 10px 0;
+        }
+    </style>
+</head>
+<body>
+    <!-- Menu delgado -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container">
+            <a class="navbar-brand" href="#"><img src="logo.png" alt="logo"></a>
+            <p style="color:white;">Sistema contra la violencia</p>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Contactos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Acerca de nosotros</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <!-- Línea blanca sin margen -->
+    <hr class="bg-white" style="margin: 0;">
+
+    <!-- Barra de navegación grande -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Sistema contra violencia</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Eventos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Información Educativa</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Leyes Normativas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Centros Locales</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Slider -->
+    <div class="container mt-4" id="slider-inicial">
+        <h1>SLIDER</h1>
+        <!-- Agrega el contenido del slider aquí -->
+    </div>
+
+    <!-- Información Educativa -->
+    <div class="container mt-4" id="informacion-educativa">
+        <h1>Información Educativa</h1>
+        <h3>Artículos</h3>
+        <h3>Videos</h3>
+        <!-- Agrega el contenido de la información educativa aquí -->
+    </div>
+
+    <!-- Eventos -->
+    <div class="container mt-4" id="eventos">
+        <h1>Eventos</h1>
+        <!-- Agrega el contenido de los eventos aquí -->
+    </div>
+
+    <!-- Leyes Normativas -->
+    <div class="container mt-4" id="leyes">
+        <h1>Leyes Normativas</h1>
+        <!-- Agrega el contenido de las leyes normativas aquí -->
+    </div>
+
+    <!-- Centros Locales -->
+    <div class="container mt-4" id="centro-local">
+        <h1>Centros Locales</h1>
+        <!-- Agrega el contenido de los centros locales aquí -->
+    </div>
+
+
+    <!-- Pie de página -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <p>&copy; 2023 Sistema Contra la Violencia</p>
+                </div>
+                <div class="col-md-6">
+                    <!-- Iconos de redes sociales -->
+                    <a href="#" class="social-icons"><i class="fab fa-facebook"></i></a>
+                    <a href="#" class="social-icons"><i class="fab fa-twitter"></i></a>
+                    <a href="#" class="social-icons"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <p>Información de contacto</p>
+                    <p>Dirección: Calle Principal, Ciudad</p>
+                    <p>Teléfono: (123) 456-7890</p>
+                    <p>Correo electrónico: info@example.com</p>
+                </div>
+                <div class="col-md-6">
+                    <p>Enlaces útiles</p>
+                    <ul>
+                        <li><a href="#">Política de privacidad</a></li>
+                        <li><a href="#">Términos de servicio</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Scripts de Bootstrap (jQuery y Popper.js) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <!-- Script de Bootstrap -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>

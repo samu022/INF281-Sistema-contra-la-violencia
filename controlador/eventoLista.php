6484c2faedca237e2526b41cbf0ed30c44c4a838
Yessid Gaston Miranda Villca <130999516+yessidMinato@users.noreply.github.com>
2023-09-19 21:29:38 -0400

    Update eventoLista.php

--- controlador/eventoLista.php
+++ controlador/eventoLista.php
@@ -1,11 +1,12 @@
 <?php
-
     include("../vista/dashboard_admin/head.php");
     include("../vista/dashboard_admin/sidebar.php");
+    include("../modelo/conexion.php");
     //incluimos modelo
     include("../modelo/EventoClase.php");
-    $car=new Evento("","","","","","");
+    $car=new Evento("","","","","","","","","","","","","");
     $res=$car->lista();
     include("../vista/Eventos/EventoLista.php");
     include("../vista/dashboard_admin/footer.php");
 ?>
+

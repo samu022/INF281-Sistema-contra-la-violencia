183d6761f8968189e1029ee0eb4c146dd55e066a
Yessid Gaston Miranda Villca <130999516+yessidMinato@users.noreply.github.com>
2023-09-19 21:26:58 -0400

    Update informacionEducativaElimina.php

--- controlador/informacionEducativaElimina.php
+++ controlador/informacionEducativaElimina.php
@@ -1,23 +1,12 @@
 <?php
-     
-
-    session_start();
-
-    if($_SESSION['privilegio'] == "lectura")
-    {
-        header("Location: ../controlador/dashboard.php");
-    }
-    else if($_SESSION['privilegio'] == "usuario" || $_SESSION['privilegio'] == "")
-    {
-        header("Location: ../controlador/login.php");
-    }
     $codInformacion=$_GET['codInformacion'];
     //$tipo=$_POST['tipo'];
     //$fecha=$_POST['fecha'];
     //$titulo=$_POST['titulo'];
     //$duracion=$_POST['duracion'];
+    include ("../modelo/conexion.php");
     include("../modelo/InformacionEducativaClase.php");
-    $carg=new InformacionEducativa($codInformacion,"","");
+    $carg=new InformacionEducativa($codInformacion,"","","","","","");
     $res=$carg->eliminar();
     if($res){
         echo "<script>

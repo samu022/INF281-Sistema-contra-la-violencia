e5fa2c2e56954bcc20a4fd87415265d5bfe72940
Yessid Gaston Miranda Villca <130999516+yessidMinato@users.noreply.github.com>
2023-09-19 21:35:57 -0400

    Add files via upload

--- controlador/eventoLista.php
+++ controlador/eventoLista.php
@@ -1,6 +1,7 @@
 <?php
     include("../vista/dashboard_admin/head.php");
     include("../vista/dashboard_admin/sidebar.php");
+    include("../modelo/conexion.php");
     //incluimos modelo
     include("../modelo/EventoClase.php");
     $car=new Evento("","","","","","","","","","","","","");

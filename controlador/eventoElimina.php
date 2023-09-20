34ec9da431d1ef977c8dd5ac69489a9d5b59e675
Yessid Gaston Miranda Villca <130999516+yessidMinato@users.noreply.github.com>
2023-09-19 21:31:45 -0400

    Update eventoElimina.php

--- controlador/eventoElimina.php
+++ controlador/eventoElimina.php
@@ -1,12 +1,12 @@
 <?php
-   
     $codEvento=$_GET['cod'];
     //$tipo=$_POST['tipo'];
     //$fecha=$_POST['fecha'];
     //$titulo=$_POST['titulo'];
     //$duracion=$_POST['duracion'];
+    include ("../modelo/conexion.php");
     include("../modelo/EventoClase.php");
-    $carg=new Evento($codEvento,"","","","","");
+    $carg=new Evento($codEvento,"","","","","","","","","","","","");
     $res=$carg->elimina();
     if($res){
         echo "<script>

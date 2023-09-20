3696c8d0afb3e2cbc780ee7a2de949692f301b8d
Yessid Gaston Miranda Villca <130999516+yessidMinato@users.noreply.github.com>
2023-09-19 21:26:22 -0400

    Update informacionEducativaModifica.php

--- controlador/informacionEducativaModifica.php
+++ controlador/informacionEducativaModifica.php
@@ -1,25 +1,34 @@
 <?php
-
-
 $codInformacion = $_GET['codInformacion'];
-
+include ("../modelo/conexion.php");
 // Incluye la clase de InformacionEducativa
 include("../modelo/InformacionEducativaClase.php");
-$informacion = new InformacionEducativa($codInformacion, "", "");
+$informacion = new InformacionEducativa($codInformacion, "", "","","","","");
 
 // Obtén la información actual de la información educativa
 $res = $informacion->lista_especifica();
 $reg = $res->fetch_assoc();
+$titulo = $reg['titulo'];
+$descripcion = $reg['descripcion'];
+$tipoViolencia = $reg['tipoViolencia'];
 $rutaDirectorio = $reg['rutaDirectorio'];
 $tipo = $reg['tipo'];
 
+
 // Guarda los valores actuales en variables temporales
+
 $rutaDirectorioActual = $rutaDirectorio;
 $tipoActual = $tipo;
+$tituloActual = $titulo;
+$descripcionActual = $descripcion;
+$tipoViolenciaActual = $tipoViolencia;
 
 include("../vista/InformacionEducativaModifica.php");
 
 if (isset($_POST['ModificarInformacion'])) {
+    $titulo = $_POST['titulo'];
+    $descripcion = $_POST['descripcion'];
+    $tipoViolencia = $_POST['tipoViolencia'];
     $tipo = $_POST['tipo'];
 
     // Verifica si se seleccionó un nuevo archivo
@@ -46,6 +55,9 @@ if (isset($_POST['ModificarInformacion'])) {
     }
 
     // Siempre actualiza el tipo, incluso si no se seleccionó un nuevo archivo
+    $informacion->setTitulo($titulo);
+    $informacion->setDescripcion($descripcion);
+    $informacion->setTipoViolencia($tipoViolencia);
     $informacion->setTipo($tipo);
 
     // Realiza la modificación en la base de datos
@@ -61,4 +73,3 @@ if (isset($_POST['ModificarInformacion'])) {
     }
 }
 ?>
-

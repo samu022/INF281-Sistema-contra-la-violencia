1ba13f424980b73269c662ad337ac9d350fcb2ab
Yessid Gaston Miranda Villca <130999516+yessidMinato@users.noreply.github.com>
2023-09-27 02:09:32 -0400

    Update panelweb.php

--- controlador/panelweb.php
+++ controlador/panelweb.php
@@ -21,5 +21,21 @@
     }
     $res3 = $leyNormativa->lista();
     $res4 = $centro->lista();
-    include("../vista/panelWeb.php");
+    if (isset($_POST['eventos'])) {
+        $_SESSION['formulario_enviado'] = 'eventos';
+        include("../vista/panelWebEventos.php");
+    } else if (isset($_POST['informacionEducativa'])) {
+        $_SESSION['formulario_enviado'] = 'informacionEducativa';
+        include("../vista/panelWebInformacionEducativa.php");
+    } else if (isset($_POST['centrosLocales'])) {
+        $_SESSION['formulario_enviado'] = 'centrosLocales';
+        include("../vista/panelWebCentrosLocales.php");
+    } else if (isset($_POST['leyes'])) {
+        $_SESSION['formulario_enviado'] = 'leyes';
+        include("../vista/panelWebLeyes.php");
+    } else {
+        // Aquí no se envió ningún formulario, mostrar los botones nuevamente
+        $_SESSION['formulario_enviado'] = '';
+        include("../vista/panelWeb.php");
+    }
 ?>

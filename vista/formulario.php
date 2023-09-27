09afebafb9e3d35eb53fb4f8777f52dc9a0cd66b
samu022 <samuelalegre2002gmail.com>
2023-09-26 23:15:10 -0400

    formulario php

---
+++ vista/formulario.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulario de evaluación de riesgos</title>
+</head>
+<body>
+    <h1>Formulario para evaluar tipo de violencia</h1>
+    
+    <!-- Aquí se incrusta el formulario de Google Forms -->
+    <iframe src="URL_DEL_FORMULARIO_GOOGLE" width="100%" height="800" frameborder="0" marginheight="0" marginwidth="0">Cargando...</iframe>
+
+    <!-- Otro contenido de tu página PHP -->
+</body>
+</html>

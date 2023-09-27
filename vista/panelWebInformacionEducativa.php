5252bdf2bd1eb3cc4c3f9bb37dd647be80a3f412
samu022 <samuelalegre2002gmail.com>
2023-09-27 11:00:57 -0400

    v2

--- vista/panelWebInformacionEducativa.php
+++ vista/panelWebInformacionEducativa.php
@@ -1,3 +1,27 @@
+<?php
+//incluimos modelo
+include("../modelo/conexion.php");
+include("../modelo/EventoClase.php");
+include("../modelo/InformacionEducativaClase.php");
+include("../modelo/Ley_NormativaClase.php");
+include("../modelo/CentroLocalClase.php");
+$informacion=new InformacionEducativa("","","","","","","");
+$eventos=new Evento("","","","","","","","","","","","","");
+$centro=new CentroLocal("","","","","");
+$leyNormativa = new Ley_Normativa("","","","","");
+$res=$eventos->lista();
+if (isset($_POST['filtrarInformacion'])) {
+    //echo "estoy aqui";
+    $tipoViolencia = $_POST['tipoViolencia'];
+    //echo $tipoViolencia;
+    $res2=$informacion->filtrarTipoViolencia($tipoViolencia);
+}
+else{
+    $res2 = $informacion->lista();
+}
+$res3 = $leyNormativa->lista();
+$res4 = $centro->lista();
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>

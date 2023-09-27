591951dc5e947afc04825c8bc81cf43d3f57f586
Yessid Gaston Miranda Villca <130999516+yessidMinato@users.noreply.github.com>
2023-09-27 02:24:39 -0400

    Update DenunciaLista.php

--- vista/Reporte_denuncias/DenunciaLista.php
+++ vista/Reporte_denuncias/DenunciaLista.php
@@ -7,48 +7,71 @@
 
 </head>
 <body>
-    <h2>Lista de Denuncias</h2>
-    <table  >
+    <div>
+        <h2>Lista de Denuncias</h2>
+        <table  >
+            
+            <tr>
+                <td>Codigo Denuncia</td>
+                <td>Tipo de Violencia</td>
+                <td>Descripcion</td>
+                <td>Fecha</td>
+                <td>Más inf.</td>
+                <td>Victimas</td>
+                <td>Agresores</td>
+                <td>Pruebas</td>
+                <td>Ubicacion</td>
+            </tr>
+            <?php
+                $cont=0;
+                while($reg=mysqli_fetch_array($res)){
+                    $cont=$cont+1; 
+                    echo "<tr>";
+                    echo "<td>".$reg['codDenuncia']."</td>";
+                    echo "<td>".$reg['tipo']."</td>";
+                    echo "<td>".$reg['descripcion']."</td>";
+                    echo "<td>".$reg['fecha']."</td>";
+                    echo "<td><a href='../controlador/denunciaInformacion.php?cod=".$reg['codDenuncia']."' btn='btn-danger' class='btn btn-danger'>Más Inf.</a></td>";
+                    echo "<td><a href='../controlador/victimaInformacion.php?cod=".$reg['codDenuncia']."'  class='btn btn-success'>Victimas</a></td>";
+                    echo "<td><a href='../controlador/agresorInformacion.php?cod=".$reg['codDenuncia']."'  class='btn btn-success'>Agresores</a></td>";
+                    echo "<td><a href='../controlador/pruebasInformacion.php?cod=".$reg['codDenuncia']."'  class='btn btn-success'>Pruebas</a></td>";
+                    echo "<td><a href='../controlador/geoInformacion.php?cod=".$reg['codDenuncia']."'  class='btn btn-success'>Ubicación</a></td>";
+                    echo "<td><a href='../controlador/denunciaElimina.php?cod=".$reg['codDenuncia']."' btn='btn-danger' class='btn btn-danger'>Eliminar</a></td>";
+                    echo "<td><a href='../controlador/denunciaModificar.php?cod=".$reg['codDenuncia']."'  class='btn btn-success'>Modificar</a></td>";
+                    echo "</tr>";
+                    
+                }
+            ?>
+        </table>
         
-        <tr>
-            <td>Codigo Denuncia</td>
-            <td>Tipo de Violencia</td>
-            <td>Descripcion</td>
-            <td>Fecha</td>
-            <td>Más inf.</td>
-            <td>Victimas</td>
-            <td>Agresores</td>
-            <td>Pruebas</td>
-            <td>Ubicacion</td>
-        </tr>
         <?php
-            $cont=0;
-            while($reg=mysqli_fetch_array($res)){
-                $cont=$cont+1; 
-                echo "<tr>";
-                echo "<td>".$reg['codDenuncia']."</td>";
-                echo "<td>".$reg['tipo']."</td>";
-                echo "<td>".$reg['descripcion']."</td>";
-                echo "<td>".$reg['fecha']."</td>";
-                echo "<td><a href='../controlador/denunciaInformacion.php?cod=".$reg['codDenuncia']."' btn='btn-danger' class='btn btn-danger'>Más Inf.</a></td>";
-                echo "<td><a href='../controlador/victimaInformacion.php?cod=".$reg['codDenuncia']."'  class='btn btn-success'>Victimas</a></td>";
-                echo "<td><a href='../controlador/agresorInformacion.php?cod=".$reg['codDenuncia']."'  class='btn btn-success'>Agresores</a></td>";
-                echo "<td><a href='../controlador/pruebasInformacion.php?cod=".$reg['codDenuncia']."'  class='btn btn-success'>Pruebas</a></td>";
-                echo "<td><a href='../controlador/geoInformacion.php?cod=".$reg['codDenuncia']."'  class='btn btn-success'>Ubicación</a></td>";
-                echo "<td><a href='../controlador/denunciaElimina.php?cod=".$reg['codDenuncia']."' btn='btn-danger' class='btn btn-danger'>Eliminar</a></td>";
-                echo "<td><a href='../controlador/denunciaModificar.php?cod=".$reg['codDenuncia']."'  class='btn btn-success'>Modificar</a></td>";
-                echo "</tr>";
-                
+            if($cont==0){
+                echo "<div style='text-align: center;' class='alert alert-danger' role='alert'>";
+                echo "<h3>No hay denuncias registradas actualmente</h3></div>";
             }
         ?>
-    </table>
-    
-    <?php
-        if($cont==0){
-            echo "<div style='text-align: center;' class='alert alert-danger' role='alert'>";
-            echo "<h3>No hay denuncias registradas actualmente</h3></div>";
-        }
-    ?>
-    <button type="button" class="btn btn-info" onclick="window.location.href='../controlador/denunciaRegistro.php'">Ingresar nueva denuncia</button>
+        <button type="button" class="btn btn-info" onclick="window.location.href='../controlador/denunciaRegistro.php'">Ingresar nueva denuncia</button>
+        <form action="../reportes/reporteDenuncia.php" method="post" accept-charset="utf-8">
+        <div class="">
+            <button type="submit" class="btn btn-success mb-2">Reporte de Denuncias</button>
+        </div>
+        </form>
+        <form action="../reportes/reporteVictimas.php" method="post" accept-charset="utf-8">
+            <div class="">
+                <button type="submit" class="btn btn-success mb-2">Reporte de victimas</button>
+            </div>
+        </form>
+        <form action="../reportes/reporteAgresores.php" method="post" accept-charset="utf-8">
+            <div class="">
+                <button type="submit" class="btn btn-success mb-2">Reporte de agresores</button>
+            </div>
+        </form>
+        <form action="../reportes/reporteDenunciaDetallada.php" method="post" accept-charset="utf-8">
+        <div class="">
+            <button type="submit" class="btn btn-success mb-2">Reporte de denuncia detallada</button>
+        </div>
+    </form>
+        </div>
+   
 </body>
 </html>

d786b45556557bcbdffefc7e1aa2869c7a6128dc
0xdiego <93290842+diegojoel301@users.noreply.github.com>
2023-10-02 17:55:55 -0400

    Create scriptsregistro.sql

---
+++ scripts/scriptsregistro.sql
@@ -0,0 +1,2 @@
+use bdcontraviolencia;
+INSERT INTO ley_normativa (nombre, fecha_promulgacion, tematica,informacion,ci) VALUES ("Ley 348", "2013/03/09", "Ley Integral para Garantizar a las Mujeres una Vida libre de Violencia","https://bolivia.unfpa.org/sites/default/files/pub-pdf/Ley_348_0_1.pdf","12345678");

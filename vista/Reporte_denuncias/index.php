96c2258873541bb15f4c9ca10f97a172a5d0f26a
samu022 <samuelalegre2002gmail.com>
2023-10-08 22:07:47 -0400

    vista registro denuncias listo

--- vista/Reporte_denuncias/index.php
+++ vista/Reporte_denuncias/index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Denuncias</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -58,8 +59,30 @@
             text-align: center;
             padding: 10px;
         }
+
+        /* Añade estilos personalizados para el formulario */
+        .form-label {
+            font-weight: bold;
+        }
+
+        .form-select,
+        .form-control {
+            width: 100%;
+            margin-bottom: 15px;
+        }
+
+        .mb-4 {
+            margin-bottom: 30px;
+        }
+
+        .btn-primary {
+            margin-right: 10px;
+        }
+
+        .btn-danger {
+            margin-left: 10px;
+        }
     </style>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
 <body>
     <header>
@@ -67,14 +90,13 @@
     </header>
 
     <nav>
-      <a href="#prueba">Prueba</a>
-      <a href="#agresor">Agresor</a>
-      <a href="#victima">Victima</a>
+        <a href="#prueba">Prueba</a>
+        <a href="#agresor">Agresor</a>
+        <a href="#victima">Victima</a>
     </nav>
     <div class="container mt-5">
         <form action="" method="POST">
             <h1 class="mb-4">Bienvenido al Registro de Denuncias</h1>
-            
 
             <div class="mb-3">
                 <label for="tipo" class="form-label">Seleccione el tipo de denuncia:</label>
@@ -97,49 +119,48 @@
                 <textarea class="form-control" name="des" id="des" rows="4" placeholder="Escriba aquí su descripción" required></textarea>
             </div>
             <div class="mb-3">
-                
-            <div class="mb-3">
-            <label for="anonima" class="form-label">¿La denuncia será anónima?</label>
-            <label>
-                <input type="radio" name="respuesta" value="si"> No
-            </label>
-            <label>
-                <input type="radio" name="respuesta" value="no"> Si
-            </label><br>
-            <label for="tes" class="form-label">Ingrese nombre del testigo:</label>
-            <input type="text" class="form-control" name="tes" id="tes" placeholder="Escriba aquí el nombre del testigo" disabled>
-
+                <label class="form-label">¿La denuncia será anónima?</label>
+                <label class="form-check-label">
+                    <input type="radio" name="respuesta" value="no"> No
+                </label>
+                <label class="form-check-label">
+                    <input type="radio" name="respuesta" value="si"> Si
+                </label>
             </div>
-            
-            <h1>Datos Victima</h1>
-            <iframe id="victima" src="../controlador/victimaRegistro.php" width="100%" height="1200px" frameborder="0"></iframe>
+            <div class="mb-3">
+                <label for="tes" class="form-label">Ingrese nombre del testigo:</label>
+                <input type="text" class="form-control" name="tes" id="tes" placeholder="Escriba aquí el nombre del testigo" disabled>
+            </div>
+
+           <h1>Datos Victima</h1>
+            <iframe id="victima" src="../controlador/victimaRegistro.php" width="100%" height="1400px" frameborder="0"></iframe>
             <h1>Datos Agresor</h1>
-            <iframe id="agresor" src="../controlador/agresorRegistro.php" width="100%" height="1250px" frameborder="0"></iframe>
+            <iframe id="agresor" src="../controlador/agresorRegistro.php" width="100%" height="1450px" frameborder="0"></iframe>
             <h1>Pruebas</h1>
-            <iframe id="prueba" src="../controlador/pruebaRegistro.php" width="100%" height="450px" frameborder="0"></iframe>
+            <iframe id="prueba" src="../controlador/pruebaRegistro.php" width="100%" height="500px" frameborder="0"></iframe>
             <h1>Ubicación</h1>
             <iframe src="../controlador/geoRegistro.php" width="100%" height="1000px" frameborder="0"></iframe>
+            
             <button type="submit" class="btn btn-primary" name="RegistrarDenuncia">Registrar Denuncia</button>
             <button type="button" class="btn btn-danger" onclick="window.location.href='../controlador/panelweb.php'">Volver</button>
         </form>
     </div>
     <div class="container">
-        
+        <!-- Contenido adicional -->
     </div>
     <script>
-    // Obtener referencias a los elementos del DOM
-    const radioRespuesta = document.getElementsByName('respuesta');
-    const campoTestigo = document.getElementById('tes'); // Reemplaza 'campoTestigo' con el ID real de tu campo
+        // Obtener referencias a los elementos del DOM
+        const radioRespuesta = document.getElementsByName('respuesta');
+        const campoTestigo = document.getElementById('tes'); // Reemplaza 'campoTestigo' con el ID real de tu campo
 
-    // Agregar un evento change a los radios de respuesta
-    for (let i = 0; i < radioRespuesta.length; i++) {
-        radioRespuesta[i].addEventListener('change', function () {
-            // Habilitar o deshabilitar el campo de testigo según la respuesta seleccionada
-            campoTestigo.disabled = radioRespuesta[1].checked; // Deshabilita si "No" está seleccionado
-        });
-    }
-</script>
-
+        // Agregar un evento change a los radios de respuesta
+        for (let i = 0; i < radioRespuesta.length; i++) {
+            radioRespuesta[i].addEventListener('change', function () {
+                // Habilitar o deshabilitar el campo de testigo según la respuesta seleccionada
+                campoTestigo.disabled = radioRespuesta[1].checked; // Deshabilita si "No" está seleccionado
+            });
+        }
+    </script>
 <script>
   window.watsonAssistantChatOptions = {
     integrationID: "b5720f5e-69c2-4db5-9923-cb183ac44279", // The ID of this integration.
@@ -153,7 +174,7 @@
     document.head.appendChild(t);
   });
 </script>
-    <footer>
+ <footer>
         © 2023 Mi Página Web de Denuncias
     </footer>
 </body>
